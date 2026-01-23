fd558879ba0de27518bb17fc53baaec6a83e7acb
1vs9ofc <tu_correo@gmail.com>
2026-01-23 14:37:49 -0300

    Web reconstruida y lista

---
+++ indice.js
@@ -0,0 +1,5 @@
+const datosTeclado = [
+  {categoria: "HARDWARE_K530", accion: "Flecha Arriba", teclas: "FN1 + W", descripcion: "Moverse por el historial"},
+  {categoria: "ANDROID_S20", accion: "Ir a Home", teclas: "Win + Enter", descripcion: "Vuelve a la pantalla principal"},
+  {categoria: "TERMUX_CMD", accion: "Limpiar Pantalla", teclas: "Ctrl + L", descripcion: "Limpia el texto visible"}
+];
